d4575bcd39c8a8bd97b494c16065e539dcfc7cc4
Arda Sevinç <ardasevinc.technical@gmail.com>
2023-07-26 06:52:03 +0300

    feat: eslint custom config for vite

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,3 +1,20 @@
 module.exports = {
-  extends: ["turbo", "prettier"]
+  env: { browser: true, es2020: true },
+
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    "turbo",
+    "prettier"
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
 };
